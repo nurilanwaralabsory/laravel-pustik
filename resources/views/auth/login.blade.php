3de1dda7969c1d43cd17f56eb26a29d834095114
ilhamquro <websitetuntong@gmail.com>
2024-03-01 10:47:32 +0700

    Membuat Tampilan Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -44,4 +44,94 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+{{-- LOGIN FORM --}}
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Login-Pustik</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="{{ asset('login-form') }}/css/style.css">
+
+	</head>
+	<body>
+	<section class="ftco-section" :status="session('status')">
+		<div class="container">
+			<div class="row justify-content-center">
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-12 col-lg-10">
+					<div class="wrap d-md-flex">
+						<div class="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-last">
+							<div class="text w-100">
+								<h2>Selamat Datang Di Pustik</h2>
+								<p>Belum Mempunyai Akun</p>
+								<a href="{{ route('register') }}" class="btn btn-white btn-outline-white">Sign Up</a>
+							</div>
+			      </div>
+						<div class="login-wrap p-4 p-lg-5">
+			      	<div class="d-flex">
+			      		<div class="w-100">
+			      			<h3 class="mb-4">Sign In</h3>
+			      		</div>
+								<div class="w-100">
+									<p class="social-media d-flex justify-content-end">
+										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a>
+										<a href="#" class="social-icon d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a>
+									</p>
+								</div>
+			      	</div>
+						<form method="POST" action="{{ route('login') }}" class="signin-form">
+                        @csrf
+                            <div class="form-group mb-3">
+                                <label class="label" :value="__('Email')" for="email">Email</label>
+                                <input type="email" name="email" class="form-control" placeholder="Email" :value="old('email')" required autofocus autocomplete="username">
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            </div>
+                            <div class="form-group mb-3">
+                                <label class="label" for="password" :value="__('Password')">Password</label>
+                                <input type="password" name="password" class="form-control" placeholder="Password" required autocomplete="current-password">
+                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                            </div>
+		            <div class="form-group">
+		            	<button type="submit" class="form-control btn btn-primary submit px-3">
+                            {{ __('Log in') }}</button>
+		            </div>
+		            <div class="form-group d-md-flex">
+		            	<div class="w-50 text-left">
+                            <label class="checkbox-wrap checkbox-primary mb-0">Remember Me
+                            <input type="checkbox" checked name="remember">
+                            <span class="checkmark"></span>
+                            </label>
+					    </div>
+					    	<div class="w-50 text-md-right">
+                                @if (Route::has('password.request'))
+                                    <a href="{{ route('password.request') }}">
+                                        {{ __('Forgot your password?') }}
+                                    </a>
+                                @endif
+							</div>
+		            </div>
+		          </form>
+		        </div>
+		      </div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="{{ asset('login-form') }}/js/jquery.min.js"></script>
+	<script src="{{ asset('login-form') }}/js/popper.js"></script>
+	<script src="{{ asset('login-form') }}/js/bootstrap.min.js"></script>
+	<script src="{{ asset('login-form') }}/js/main.js"></script>
+
+	</body>
+</html>
+
